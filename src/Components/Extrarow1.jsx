82d784ef94dca069e0cda8ab0620e3b94599dd42
Lakshmigithub-ui <seetaraman176@gmail.com>
2024-09-14 15:46:08 +0530

    Update Extrarow1.jsx

--- src/Components/Extrarow1.jsx
+++ src/Components/Extrarow1.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import AOS styles
 
-import Extrarow_image1 from "./Assets/extrarow_image1.avif";
+import office from "./Assets/office.avif";
 
 const Extrarow1 = () => {
   useEffect(() => {
@@ -15,21 +15,29 @@ const Extrarow1 = () => {
         className="lg:w-1/2 mb-8 lg:mb-0"
         data-aos="fade-up" // Animate the text when it scrolls into view
       >
-        <p className="text-xl font-light mb-4">We Design Delightful Customer Experience</p>
-        <h2 className="text-2xl font-semibold leading-tight">
-          We partner with brands, agencies, and start-ups to create engaging content and meaningful experiences in the digital space.
+        <h2 className="text-3xl font-semibold leading-tight">
+         About Us
         </h2>
+        <br></br>
+        <p className="text-lg font-light mb-4">
+ASM Infotech, based in Chennai, is a creative agency specializing in responsive web development and online marketing. We offer web-based IT solutions, including website design, CMS, eCommerce, web applications, SEO, SEM, and digital marketing. Our services also include social media marketing, mobile app development, graphic design, and web hosting.
+
+We are committed to delivering customized solutions that meet our clients' unique needs and goals. From simple corporate websites to complex eCommerce platforms, we create tailor-made web solutions. As a local agency, we prioritize clear, interactive communication, ensuring we're always accessible to our clients.</p>
+
       </div>
+
       <div
         className="lg:w-1/2 flex justify-center items-center"
         data-aos="fade-down" // Animate the image when it scrolls into view
       >
         <img
           className="w-full h-auto rounded-lg shadow-lg transition-transform transform hover:scale-105"
-          src={Extrarow_image1}
+          src={office}
           alt="Design"
         />
       </div>
+
+
     </div>
   );
 }
