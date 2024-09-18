a4ec44ef6a12db1527d5eee0f87e94524f9f41e3
Lakshmigithub-ui <seetaraman176@gmail.com>
2024-09-18 19:17:42 +0530

    Update Extrarow1.jsx

--- src/Components/Extrarow1.jsx
+++ src/Components/Extrarow1.jsx
@@ -10,16 +10,16 @@ const Extrarow1 = () => {
   }, []);
 
   return (
-    <div className="bg-slate-800 text-white py-12 px-6 lg:px-16 flex flex-col lg:flex-row items-center justify-between">
+    <div className="bg-slate-800 text-slate-200 py-12 px-6 lg:px-16 flex flex-col lg:flex-row items-center justify-between">
       <div
         className="lg:w-1/2 mb-8 lg:mb-0"
         data-aos="fade-up" // Animate the text when it scrolls into view
       >
-        <h2 className="text-3xl font-semibold leading-tight">
+        <h2 className="text-2xl font-semibold leading-tight">
          About Us
         </h2>
         <br></br>
-        <p className="text-lg font-light mb-4">
+        <p className="text-lg font-thin mb-4">
 ASM Infotech, based in Chennai, is a creative agency specializing in responsive web development and online marketing. We offer web-based IT solutions, including website design, CMS, eCommerce, web applications, SEO, SEM, and digital marketing. Our services also include social media marketing, mobile app development, graphic design, and web hosting.
 
 We are committed to delivering customized solutions that meet our clients' unique needs and goals. From simple corporate websites to complex eCommerce platforms, we create tailor-made web solutions. As a local agency, we prioritize clear, interactive communication, ensuring we're always accessible to our clients.</p>
@@ -31,7 +31,7 @@ We are committed to delivering customized solutions that meet our clients' uniqu
         data-aos="fade-down" // Animate the image when it scrolls into view
       >
         <img
-          className="w-full h-auto rounded-lg shadow-lg transition-transform transform hover:scale-105"
+          className="w-full h-auto rounded-lg shadow-lg transition-transform transform"
           src={office}
           alt="Design"
         />
